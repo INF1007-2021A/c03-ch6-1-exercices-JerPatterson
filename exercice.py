1e3fcaa7f4793d6f20cff9ac7ed9ea0dabbe1d46
JerPatterson <you@example.com>
2021-09-27 21:38:02 -0400

    exercises... last one remains

--- exercice.py
+++ exercice.py
@@ -5,33 +5,60 @@
 def order(values: list = None) -> list:
     if values is None:
         # TODO: demander les valeurs ici
-        pass
-
-    return []
+        values = list(map(str, input('Entrez dix valeurs: ').split()))
+        values = values[:10]
+    return sorted(values)
 
 
 def anagrams(words: list = None) -> bool:
     if words is None:
         # TODO: demander les mots ici
-        pass
-
-    return False
+        words = sorted( list( map(str, input('Entrez deux mots: ').split()) ) )
+    word1 , word2 = set(words[0]) , set(words[1])
+    return True if len(word1.difference(word2)) == 0 else False
 
 
 def contains_doubles(items: list) -> bool:
-    return False
+    number_ofItems = len(items)
+    number_noDuplicates = len(set(items))
+    return True if number_ofItems != number_noDuplicates else False
 
 
 def best_grades(student_grades: dict) -> dict:
     # TODO: Retourner un dictionnaire contenant le nom de l'étudiant ayant la meilleure moyenne ainsi que sa moyenne
-    return {}
+    class_best = {}
+    averages = []
+    names = []
+    for student, tests in student_grades.items() :
+        number_ofTests = len(tests)
+        grades_sum = 0
+        for grades in tests :
+            grades_sum += grades
+        average = grades_sum / number_ofTests
+        averages.append(average)
+        names.append(student)
+    best_average = max(averages)
+    best_student = names[averages.index(best_average)]   
+    class_best[best_student] = best_average
+
+    return class_best
 
 
 def frequence(sentence: str) -> dict:
     # TODO: Afficher les lettres les plus fréquentes
     #       Retourner le tableau de lettres
-
-    return {}
+    with5_appearence = {}
+    letters = set(sentence)
+    characters = list(sentence)
+    for letter in letters :
+        appearence = characters.count(letter)
+        if appearence > 5 :
+            with5_appearence[letter] = appearence
+        else : 
+            continue
+    with5_appearence = dict(sorted(with5_appearence.items(), key=lambda index: index[1], reverse=True))
+    print(with5_appearence)
+    return with5_appearence
 
 
 def get_recipes():
