1ae814f522f7f79956cad1dfc6a8bda5e62322c9
JerPatterson <you@example.com>
2021-10-03 09:39:20 -0400

    all exercises working but can be simplify

--- exercice.py
+++ exercice.py
@@ -1,47 +1,38 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
 def order(values: list = None) -> list:
     if values is None:
         # TODO: demander les valeurs ici
         values = list(map(str, input('Entrez dix valeurs: ').split()))
         values = values[:10]
-    return sorted(values)
 
+    return sorted(values)
 
 def anagrams(words: list = None) -> bool:
     if words is None:
         # TODO: demander les mots ici
-        words = sorted( list( map(str, input('Entrez deux mots: ').split()) ) )
-    word1 , word2 = set(words[0]) , set(words[1])
-    return True if len(word1.difference(word2)) == 0 else False
+        words = list(map(str, input('Entrez deux mots: ').split()))
+    word1 , word2 = words[0] , words[1]
+    return sorted(word1) == sorted(word2)
 
 
 def contains_doubles(items: list) -> bool:
-    number_ofItems = len(items)
-    number_noDuplicates = len(set(items))
-    return True if number_ofItems != number_noDuplicates else False
+    return False if len(items) == len(set(items)) else True
 
 
 def best_grades(student_grades: dict) -> dict:
     # TODO: Retourner un dictionnaire contenant le nom de l'étudiant ayant la meilleure moyenne ainsi que sa moyenne
-    class_best = {}
     averages = []
     names = []
     for student, tests in student_grades.items() :
-        number_ofTests = len(tests)
-        grades_sum = 0
-        for grades in tests :
-            grades_sum += grades
-        average = grades_sum / number_ofTests
+        average = sum(tests) / len(tests)
         averages.append(average)
         names.append(student)
     best_average = max(averages)
     best_student = names[averages.index(best_average)]   
-    class_best[best_student] = best_average
 
-    return class_best
+    return {best_student : best_average}
 
 
 def frequence(sentence: str) -> dict:
@@ -52,23 +43,29 @@ def frequence(sentence: str) -> dict:
     characters = list(sentence)
     for letter in letters :
         appearence = characters.count(letter)
-        if appearence > 5 :
+        if appearence > 5 : #Erreur dans le fichier test pour cette contrainte...
             with5_appearence[letter] = appearence
-        else : 
-            continue
     with5_appearence = dict(sorted(with5_appearence.items(), key=lambda index: index[1], reverse=True))
-    print(with5_appearence)
+
     return with5_appearence
 
 
 def get_recipes():
     # TODO: Demander le nom d'une recette, puis ses ingredients et enregistrer dans une structure de données
-    pass
+    recipes = {}
+    recipe = str(input("Entrez le nom d'une recette: "))
+    ingredients = list(map(str, input('Entrez les ingrédients de cette recette: ').split()))
+    recipes[recipe] = set(ingredients)
 
+    return recipes
 
-def print_recipe(ingredients) -> None:
+def print_recipe(recipes) -> None: # Recipes déjà dictionnaire ....
     # TODO: Demander le nom d'une recette, puis l'afficher si elle existe
-    pass
+    recipe_choice = input('Choisissez une recette: ')
+    if recipe_choice in recipes :
+        print(str(recipes[recipe_choice]))
+    else :
+        print(f"La recette {recipe_choice} n'est pas disponible")
 
 
 def main() -> None:
